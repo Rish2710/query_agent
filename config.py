8c59aeca83586ecf286de57c09f7f971af8393be
Rishabh Rastogi <rrishabh@bizdata360.com>
2025-02-16 12:36:58 +0530

    Updated API Key and webhook url

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 GINA_API_URL = "https://api.jina.ai/v1/embeddings"
-GINA_API_KEY = "jina_825d77c8abc442b68fb0552cc9vV9ljBlyL79YspK6n"
+GINA_API_KEY = "jina_825d77c8abc442b68fb0552cca742d053su_PKAvB59vV9ljBlyL79YspK6n"
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/T08D2QMDL0L/B0RQQ/dKsMABt9IbqzIJ"
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 
 CHROMADB_PATH = "data/chromadb_store"
